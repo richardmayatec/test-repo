49a65a1ad3e63e78f46211982e2cd648783b1b9b
richard-maya <richard.maya94@gmail.com>
2023-04-12 14:52:44 -0600

    Default Route after Logged In

--- login-test/routes/web.php
+++ login-test/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('tablero', function(){
+    return view('test');
+})->middleware(['auth'])->name('tablero');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
